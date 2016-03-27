615e02dea44f77ff2d019335bcaadced2251b1f9
zmartl-bbc <luca.marti@bluewin.ch>
2016-03-27 15:52:39 +0200

    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

--- includes/head.php
+++ includes/head.php
@@ -15,5 +15,5 @@
 	rel='stylesheet' type='text/css'>
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<title>Praxis Witzig</title>
+<title>Malatelier Wiwize</title>
 </head>
